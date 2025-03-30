7f54da4eef0bdbc716112238a8e370ab2c4f5c28
daothao03 <daothao.gau2003@gmail.com>
2025-03-30 21:54:26 +0700

    setup project fe

---
+++ UI-Admin/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <h1 class="text-3xl font-bold underline text-blue-600">
+                Hello world!
+            </h1>
+        </>
+    );
+}
+
+export default App;
