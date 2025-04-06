3f58a5c50168f91f8846eeedbe2089d7f9a199d2
daothao03 <daothao.gau2003@gmail.com>
2025-04-06 23:33:37 +0700

    login page

--- ui-uniportal/src/pages/_app.tsx
+++ ui-uniportal/src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from "next/app";
+import "../styles/global.css";
+import "../styles/base.css";
 
 export default function App({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
--- ui-uniportal/src/pages/index.tsx
+++ ui-uniportal/src/pages/index.tsx
@@ -1,5 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
