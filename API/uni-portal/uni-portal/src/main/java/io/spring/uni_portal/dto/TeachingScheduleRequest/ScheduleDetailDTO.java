49ceb26ab9ab78911a97c7e646d23704c6c93b08
yenhoang65 <hoangsyyen123@gmail.com>
2025-05-01 02:09:57 +0700

    no commit

--- API/uni-portal/uni-portal/src/main/java/io/spring/uni_portal/dto/TeachingScheduleRequest/ScheduleDetailDTO.java
+++ API/uni-portal/uni-portal/src/main/java/io/spring/uni_portal/dto/TeachingScheduleRequest/ScheduleDetailDTO.java
@@ -1,53 +1,53 @@
-//package io.spring.uni_portal.dto.TeachingScheduleRequest;
-//
-//import java.time.LocalDateTime;
-//
-//public class ScheduleDetailDTO {
-//    private Long classroomId;
-//    private Long lesson;
-//    private LocalDateTime createdAt;
-//    private LocalDateTime dateTime;
-//    private String classType; // 'ltCredits' hoặc 'thCredits'
-//
-//    // Getters and Setters
-//    public Long getClassroomId() {
-//        return classroomId;
-//    }
-//
-//    public void setClassroomId(Long classroomId) {
-//        this.classroomId = classroomId;
-//    }
-//
-//    public Long getLesson() {
-//        return lesson;
-//    }
-//
-//    public void setLesson(Long lesson) {
-//        this.lesson = lesson;
-//    }
-//
-//    public LocalDateTime getCreatedAt() {
-//        return createdAt;
-//    }
-//
-//    public void setCreatedAt(LocalDateTime createdAt) {
-//        this.createdAt = createdAt;
-//    }
-//
-//    public LocalDateTime getDateTime() {
-//        return dateTime;
-//    }
-//
-//    public void setDateTime(LocalDateTime dateTime) {
-//        this.dateTime = dateTime;
-//    }
-//
-//    public String getClassType() {
-//        return classType;
-//    }
-//
-//    public void setClassType(String classType) {
-//        this.classType = classType;
-//    }
-//}
-//
+package io.spring.uni_portal.dto.TeachingScheduleRequest;
+
+import java.time.LocalDateTime;
+
+public class ScheduleDetailDTO {
+    private Long classroomId;
+    private Long lesson;
+    private LocalDateTime createdAt;
+    private LocalDateTime dateTime;
+    private String classType; // 'ltCredits' hoặc 'thCredits'
+
+    // Getters and Setters
+    public Long getClassroomId() {
+        return classroomId;
+    }
+
+    public void setClassroomId(Long classroomId) {
+        this.classroomId = classroomId;
+    }
+
+    public Long getLesson() {
+        return lesson;
+    }
+
+    public void setLesson(Long lesson) {
+        this.lesson = lesson;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getClassType() {
+        return classType;
+    }
+
+    public void setClassType(String classType) {
+        this.classType = classType;
+    }
+}
+
