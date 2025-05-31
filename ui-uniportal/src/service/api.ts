1d61b87df56d0ba51ca7f40c299322075f9601e0
daothao03 <daothao.gau2003@gmail.com>
2025-05-31 22:15:18 +0700

    update main api

--- ui-uniportal/src/service/api.ts
+++ ui-uniportal/src/service/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://3.27.199.170:8080/api/",
+    // baseURL: "http://3.27.199.170:8080/api/",
+    baseURL: "https://uniportal.site/api/api/",
 });
 
 export default api;
